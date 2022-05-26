47f5183c1eca4d2b05e4e0dd5bd92f603df752bc
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-05-25 22:21:58 -0400

    Added more strtucture and elements for the ui task. The script have an issue with the event listener into an event listener

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-console.log("WELCOME TO ROCK PAPER SCISSORS GAME")
+//console.log("WELCOME TO ROCK PAPER SCISSORS GAME")
 
 function playRound(user) { // this function gets user option and manages the whole gameplay
-    // e.stopPropagation();
+    //e.stopPropagation();
     //let user = e.classList.value;
 
-    console.log("User: " + user);
+    //console.log("User: " + user);
     
     let cpu = computerPlay();
-    console.log("Cpu: " + cpu);
+    //console.log("Cpu: " + cpu);
 
-    let result = match(cpu, user);
-    console.log((result != 'TIE') ? "!!! User " + result + " !!!" : "!!! Result was a TIE !!!");
+    let result = match(cpu.toLowerCase(), user.toLowerCase());
+    //console.log((result != 'TIE') ? "!!! User " + result + " !!!" : "!!! Result was a TIE !!!");
 
-    return (result != 'TIE') ? "!!! User " + result + " !!!" : "!!! Result was a TIE !!!";
+    return result;
 }
 
 function computerPlay() { // this function gets a random play for cpu
@@ -22,20 +22,31 @@ function computerPlay() { // this function gets a random play for cpu
 }
 
 function match(cpu, user) {
-    let result = (cpu == user) ? "TIE":
-    (cpu == 'rock') ? (user == 'paper') ? 'WIN' : 'LOSE' :
-    (cpu == 'paper') ? (user == 'scissors') ? 'WIN' : 'LOSE' :
-    (cpu == 'scissors') ? (user == 'rock') ? 'WIN' : 'LOSE' : 'LOSE';
+    let result = (cpu == user) ? "tie":
+    (cpu == 'rock') ? (user == 'paper') ? 'win' : 'lose' :
+    (cpu == 'paper') ? (user == 'scissors') ? 'win' : 'lose' :
+    (cpu == 'scissors') ? (user == 'rock') ? 'win' : 'lose' : 'lose';
 
     return result;
 }
 
-const buttons = document.querySelectorAll('button');
-// console.log(buttons);
+const start = document.querySelector('.start-end');
+const buttons = document.querySelectorAll('.user-panel > button');
+//console.log(start);
+//console.log(buttons);
 
-buttons.forEach(button => button.addEventListener('click', () => {
-    let output = playRound(button.innerHTML);
-    document.getElementById("result").innerHTML = output;
+start.addEventListener('click', () => {
+    let game = true;
+    let round = 0;
 
+    while (round < 5) {
+        //console.log(round);
+        buttons.forEach(button => button.addEventListener('click', () => {
+            let output = playRound(button.innerHTML);
+            document.getElementById("result").innerHTML = output;
 
-}));
+            round++;
+        }));
+    }
+    //console.log("OUTTTTTT");
+});
